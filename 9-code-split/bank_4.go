d447e2c6267b54a232b50da5d9ee7be81ef3f21f
ng1n3 <code.with.muyiwa@gmail.com>
2024-11-15 18:15:53 +0100

    converting functions to generics ones

--- 9-code-split/bank_4.go
+++ 9-code-split/bank_4.go
@@ -9,32 +9,32 @@ import (
 
 const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() (float64, error) {
-	data, err := os.ReadFile(accountBalanceFile)
+func getFloatFromFile(fileName string) (float64, error) {
+	data, err := os.ReadFile(fileName)
 
 	if err != nil {
-		return 1000, errors.New("failed to find balance flie.")
+		return 1000, errors.New("failed to find flie")
 	}
 
-	balanceText := string(data)
-	balance, err := strconv.ParseFloat(balanceText, 64)
+	valueText := string(data)
+	value, err := strconv.ParseFloat(valueText, 64)
 
 	if err != nil {
-		return 1000, errors.New("Failed to parse stored balance value.")
+		return 1000, errors.New("failed to parse stored value")
 	}
 
-	return balance, nil
+	return value, nil
 
 }
 
-func writeBalanceToFile(balance float64) {
-	balanceText := fmt.Sprint(balance)
-	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+func writeFloatToFile(value float64, fileName string) {
+	valueText := fmt.Sprint(value)
+	os.WriteFile(accountBalanceFile, []byte(valueText), 0644)
 
 }
 
 func main() {
-	var accountBalance, err = getBalanceFromFile()
+	var accountBalance, err = getFloatFromFile(accountBalanceFile)
 
 	if err != nil {
 		fmt.Println("ERROR")
@@ -68,7 +68,7 @@ func main() {
 
 			accountBalance += depositAmount
 			fmt.Println("Balance Updated! New amount: ", accountBalance)
-			writeBalanceToFile(accountBalance)
+			writeFloatToFile(accountBalance, accountBalanceFile)
 		case 3:
 			fmt.Println("How much do you want to withdraw: ")
 			var withdrawAmount float64
@@ -86,7 +86,7 @@ func main() {
 
 			accountBalance -= withdrawAmount
 			fmt.Println("Balance updated! New balance: ", accountBalance)
-			writeBalanceToFile(accountBalance)
+			writeFloatToFile(accountBalance, accountBalanceFile)
 		default:
 			fmt.Println("Goodbye!")
 			fmt.Println("Thanks for reaching our Bank.")
