2fda624217781a2b074355f6a3cbbedbc9d630f5
ng1n3 <code.with.muyiwa@gmail.com>
2024-11-17 09:25:44 +0100

    validation with structs

--- 11-struct/structs.go
+++ 11-struct/structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -23,17 +24,31 @@ func (user *User) clearUserName() {
   user.lastName = ""
 }
 
+func newuser(firstName, lastName, birthdate string) (*User, error) {
+  if firstName == "" || lastName == "" || birthdate == "" {
+    return nil, errors.New("first name, last name, birthdate are required")
+  }
+
+
+  return &User{
+    firstName: firstName,
+    lastName: lastName,
+    birthDate: birthdate,
+    createdAt: time.Now(),
+  }, nil
+}
+
 // func outputUserDetails(user *User) {
-  
 //   fmt.Println((*user).firstName, user.lastName, user.birthDate)
 // }
+
 
 func main() {
   userFirstName := getUserData("please enter your first name: ")
   userLastName := getUserData("Please enter your last name: ")
   userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYY): ")
 
-  var appUser User
+  var appUser *User
 
   // appUser = User{
   //   firstName: userFirstName,
@@ -42,17 +57,21 @@ func main() {
   //   createdAt: time.Now(),
   // }
   // appUser = User{}
-
-  appUser = User{
-    userFirstName,
-    userLastName,
-    userBirthdate,
-    time.Now(),
-  }
-
+  // appUser = User{
+  //   userFirstName,
+  //   userLastName,
+  //   userBirthdate,
+  //   time.Now(),
+  // }
   // ... do something awesome with that generated data!
-
   // outputUserDetails(&appUser)
+
+  appUser, err  := newuser(userFirstName, userLastName, userBirthdate)
+
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
 
   appUser.outputUserDetails()
   appUser.clearUserName()
@@ -63,6 +82,6 @@ func main() {
 func getUserData(prompText string) string {
   fmt.Print(prompText)
   var value string
-  fmt.Scan(&value)
+  fmt.Scanln(&value)
   return value
 }
