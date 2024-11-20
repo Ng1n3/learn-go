3319bfb2ea0777821df6a9d7ca80ed781b839a61
ng1n3 <code.with.muyiwa@gmail.com>
2024-11-20 18:25:43 +0100

    make with maps

--- 17-make-function/main.go
+++ 17-make-function/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+  fmt.Println(m)
+}
+
 func main() {
   userNames := make([]string, 2, 5)
 
@@ -10,4 +16,14 @@ func main() {
   userNames = append(userNames, "Manuel")
 
   fmt.Println(userNames)
+
+  courseRating := make(floatMap, 3)
+
+  courseRating["go"] = 4.8
+  courseRating["react"] = 4.5
+  courseRating["angular"] = 4.7
+  courseRating["node"] = 4.9
+
+  courseRating.output()
+  fmt.Println(courseRating)
 }
