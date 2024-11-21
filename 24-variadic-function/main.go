6c613e44ad2eb1a14a4b311822fc646f74f3a231
ng1n3 <code.with.muyiwa@gmail.com>
2024-11-21 23:38:29 +0100

    splitting slices into parameter values

--- 24-variadic-function/main.go
+++ 24-variadic-function/main.go
@@ -3,18 +3,20 @@ package main
 import "fmt"
 
 func main() {
-  // numbers := []int{1, 10, 15}
-  sum := sumup(1, 10, 15, 28 -50)
+	numbers := []int{1, 10, 15}
+	sum := sumup(1, 10, 15, 28-50)
+	anotherSum := sumup(1, numbers...)
 
-  fmt.Println(sum)
+	fmt.Println(sum)
+	fmt.Println(anotherSum)
 }
 
-func sumup(numbers ...int) int {
-  sum := 0
+func sumup(startingNumbers int, numbers ...int) int {
+	sum := 0
 
-  for _, val := range numbers {
-    sum += val
-  }
+	for _, val := range numbers {
+		sum += val
+	}
 
-  return sum
+	return sum
 }
