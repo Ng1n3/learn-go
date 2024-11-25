092a1dba8b330287a695b1b94ac79e704ae6b866
ng1n3 <code.with.muyiwa@gmail.com>
2024-11-25 13:43:47 +0100

    go routines intro

--- 25-price-calculator/main.go
+++ 25-price-calculator/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
-	"price-calculator/filemanager"
+	"price-calculator/cmdmanager"
+	// "price-calculator/filemanager"
 	"price-calculator/prices"
 )
+
+
 
 func main() {
 	taxRates := []float64{0, 0.7, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("price.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		priceJob.Process()
+		// fm := filemanager.New("price.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
+		err := priceJob.Process()
+
+		if err != nil {
+			fmt.Println("Could not process job")
+			fmt.Println(err)
+		}
 	}
 }
